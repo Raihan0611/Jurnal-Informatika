6698c949bd3dcd51b220f93a8411c22a0d011cff
Raihan <134850471+Raihan0611@users.noreply.github.com>
2024-11-05 21:43:01 +0700

    Add files via upload

--- Tugas12.py
+++ Tugas12.py
@@ -1,27 +1,49 @@
-while True:
-    usia = int(input("Masukkan umur: "))
-    if usia <= 0 :
-        print("Belum lahir")
-    elif usia < 13 :
-        print ("Kategori: Anak-anak")
-    elif usia <= 18 :
-        print ("Kategori: Remaja")
-    elif usia <= 50 :
-        if usia < 30 :
-            print ("Kategori: Dewasa Muda")
-        else:
-            print ("Kategori: Dewasa")
-    else:
-        print ("Kategori: Lansia")
+# harga_belanja = float(input("Masukkan harga: "))
+# status = str(input("member atau non-member? "))
 
-    nilai = int(input("Masukkan nilai: "))
-    hadir = int(input("Masukkan presentase kehadiran: "))
-    if (nilai >= 75) and (hadir >= 80):
-        print("Lulus")
-    elif nilai < 75 :
-        if hadir > 90 :
-            print("Perbaiki nilai ujian")
-        else:
-            print("Tidak Lulus")
+# if status == "member" :
+#     if harga_belanja > 1000000 :
+#         diskon1 = harga_belanja*0.20
+#         print("Selamat, anda mendapatkan diskon 20%!")
+#         print("Total belanja anda menjadi Rp,",harga_belanja - diskon1)
+#     elif harga_belanja >= 500000 :
+#         diskon2 = harga_belanja*0.15
+#         print("Selamat, anda mendapatkan diskon 15!")
+#         print("Total belanja anda menjadi Rp,", harga_belanja - diskon2)
+#     else:
+#         diskon3 = harga_belanja*0.10
+#         print("Selamat, anda mendapatkan diskon 10%!")
+#         print("Total belanja anda menjadi Rp,", harga_belanja - diskon3)
+
+# elif status == "non-member" :
+#     if harga_belanja > 1000000 :
+#         diskon1 = harga_belanja*0.10
+#         print("Selamat, anda mendapatkan diskon 10%!")
+#         print("Total belanja anda menjadi Rp,",harga_belanja - diskon1)
+#     elif harga_belanja >= 500000 :
+#         diskon2 = harga_belanja*0.05
+#         print("Selamat, anda mendapatkan diskon 5%!")
+#         print("Total belanja anda menjadi Rp,", harga_belanja - diskon2)
+#     else:
+#         print("Tidak ada diskon!")
+#         print("Total belanja anda menjadi Rp,", harga_belanja)
+# else:
+# print("Terima kasih telah berbelanja, selamat datang kembali!")
+
+
+jenis_kendaraan = str(input("Motor atau Mobil?: "))
+durasi_parkir = float(input("Berapa lama anda akan parkir?: "))
+
+if jenis_kendaraan == "motor" :
+    if durasi_parkir <= 2 :
+        print("Tarif parkir anda adalah Rp,", 2000)
     else:
-        print("Tingkatkan kehadiran")
+        print("Tarif parkir anda adalah Rp,", 2000+(durasi_parkir-2)*1000)
+elif jenis_kendaraan == "mobil" :
+    if durasi_parkir <= 2 :
+        print("Tarif parkir anda adalah Rp,", 5000)
+    else:
+        print("Tarif parkir anda adalah Rp,", 5000+(durasi_parkir-2)*2000)
+else:
+    print("Kendaraan tidak Valid")
+        
